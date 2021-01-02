f4eea4dbcbe7a5565fc7fdba972181855820fc45
ericabyss <erica051746@naver.com>
2021-01-02 22:57:12 +0900

    0102_h

--- src/pages/feed/FeedContainer.tsx
+++ src/pages/feed/FeedContainer.tsx
@@ -1,19 +1,61 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 //import { Layout } from "..";
 import FeedPresenter from "./FeedPresenter";
 
 const FeedContainer = () => {
 
     const scrollEl = useRef(null);
+    const [count, setCount] = useState<number>(12);
+    const [loading, setLoading] = useState(false);
+
+
+
+    const handleTimer = () => {
+        setTimeout(() => {
+            setCount(count+4);
+            setLoading(false);
+        }, 1000);
+        
+
+    };
 
     const handleScroll = () => {
-        console.log("scroll");
-    }
 
-    console.log(scrollEl);
-    console.log(document.getElementById("wrapper"));
+        const { scrollHeight, scrollTop, clientHeight }:any = scrollEl.current;
 
-    return <FeedPresenter scrollEl = {scrollEl} handleScroll={handleScroll}/>;
+            console.log(
+                "scrollHeight : ",
+                scrollHeight,
+                "scrollTop : ",
+                scrollTop,
+                "clientHeight : ",
+                clientHeight
+                );
+
+        if (scrollTop+clientHeight >= scrollHeight -20){
+            setLoading(true);
+            handleTimer();
+        }
+    };
+
+
+    useEffect(()=> {
+
+        document.getElementById("wrapper").addEventListener("scroll", handleScroll);
+
+        return () => {
+            document.getElementById("wrapper").removeEventListener("scroll", handleScroll); //이벤트 사용시 구독해제 필요
+        };
+    }); //자바스크립트 반반
+
+
+    return(
+        <FeedPresenter 
+           scrollEl={scrollEl} 
+           handleScroll={handleScroll} 
+           count={count}
+           loading={loading}
+       />);
 };
 
 export default FeedContainer;
--- src/pages/feed/FeedPresenter.tsx
+++ src/pages/feed/FeedPresenter.tsx
@@ -17,33 +17,44 @@ grid-gap: 10px;
 `;
 
 const ImgBox = styled.img`
-    width: 100%
+    width: 100%;
     border-radius: 10px;
+`;
+
+const Overlay = styled.div`
+    position: absolute;
+    z-index: 10;
+    background-color : rgba(0, 0, 0, 0.8);
+    width : 100%;
+    height: 100%;
 `;
 
 interface IProps{
     scrollEl : MutableRefObject<any>;
     handleScroll: () => void;
+    count: number;
+    loading: boolean;
 }
 
 const FeedPresenter = (props: IProps) => {
     return(
      <Fragment>
+         {props.loading ? <Overlay /> : null }
          <Wrapper 
             id="wrapper"
             ref={props.scrollEl}
-            onScroll={()=>console.log("scroll", props.scrollEl)}
          >
             <GridBox>
             {/*map함수 = >for이랑 같은 반복문 배열의 length만큼 반복해주는함수*/}
             {img.map((element,index) => {
+                if(index<props.count){
                 return <ImgBox src={element.src} alt ="" />;
+                }
             })}
             </GridBox>
         </Wrapper>
     </Fragment>
     );
 };
-
 
 export default FeedPresenter;
