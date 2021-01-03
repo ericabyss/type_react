7275ab762298ccfd07163dd9506bfa0120e66e26
ericabyss <erica051746@naver.com>
2021-01-03 21:13:51 +0900

    0103_h

--- src/pages/feed/FeedPresenter.tsx
+++ src/pages/feed/FeedPresenter.tsx
@@ -24,7 +24,7 @@ const ImgBox = styled.img`
 const Overlay = styled.div`
     position: absolute;
     z-index: 10;
-    
+    background-color : rgba(0, 0, 0, 0.8);
     width : 100%;
     height: 100%;
 `;
