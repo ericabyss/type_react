e2483eafe54ba83bbf9a51fd0d1c91e83ba6e40b
ericabyss <erica051746@naver.com>
2021-01-05 23:51:15 +0900

    q

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -41,7 +41,7 @@ export default class CardCompnent extends Component{
                   <CardItem>
                     <Text>
                       <Text style={{ fontWeight:'900'}}>Anpigon</Text>
-                        이번에는 리액트 네이티브(React Native)로 인스타그램 UI을 구현하는 포스팅입니다. 다른 앱을 따라 만들어 보는 것은 굉장히 재미있습니다. 구글에서 인스타그램 클론 코딩 강의를 찾아보니, 다른 개발자들이 올린 동영상 강의를 몇 개 찾을 수 있었습니다.
+                        aaaaaa
                       </Text>
                     </CardItem>
                   </Card>
