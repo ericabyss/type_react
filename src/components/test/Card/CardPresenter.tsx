a5812327cd0597c5edc9b4ee4b6b8846da6c227f
ericabyss <erica051746@naver.com>
2021-01-21 00:06:10 +0900

    1

--- src/components/test/Card/CardPresenter.tsx
+++ src/components/test/Card/CardPresenter.tsx
@@ -71,7 +71,7 @@ const CardPresenter = (props: ICard) => {
                     <div className={classes.prag_c}>
                         {props.comments.map((comment, index) => (
                             /*<Box display={"flex"} flex-direction={"column"}>*/
-                            <span text-overflow={"ellipsis"}><ul key={index}><li className={classes.line}><span className={classes.com1}>gamzya </span>&nbsp; {comment}</li> </ul></span>
+                            <ul key={index}><li className={classes.line}><span className={classes.com1}>gamzya </span>&nbsp; <span text-overflow={"ellipsis"}>{comment}</span></li> </ul>
                             /*</Box>*/
                             ))}
                     </div>
